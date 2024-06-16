4bd37df94058d8ea03e767105f5badfab28879d3
lmd2610 <laminhduc2610@gmail.com>
2024-06-16 12:29:45 +0700

    init

---
+++ server/middleware/middleware.ts
@@ -0,0 +1,12 @@
+import { verifyJwt } from "../helpers/jwt"
+
+function authenticateToken(req:any, res:any, next:any) {
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+  
+    if (token == null) return res.sendStatus(401)
+  
+    let result = verifyJwt(token);
+    return result
+  }
+  export {authenticateToken as Middleware}
